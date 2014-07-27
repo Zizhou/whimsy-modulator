82b4277e8064d507c0b77057514e876c8d1e7c12
Daniel <dan.t.ching@gmail.com>
2014-07-26 22:28:15 -0700

    put_sentence functions-ish

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
-import random
+from django.contrib.auth.models import User, AnonymousUser
+import random, re
+
+
 
 # Create your models here.
 
@@ -20,5 +22,21 @@ class Sentence(models.Model):
     #the meat of this app, returns a generated sentence terminated by a period
     def get_markov_sentence(self):
         #TODO actually write this, lol
+
+        
         output = 'butts, lol'
         return output
+
+    def put_sentence(self, data, user):
+        if data == '' or len(data) > 299:
+            return False
+        else:
+            if re.search(r'[?!.]', data) == None:
+                data = data + '.'
+            self.sentence = data
+            try:
+                self.submitter = user
+            except:
+                return False
+            self.save()
+            return True
